7ad224daa725193fbd69a9f0448e8646537f2274
Edward Langley <el-github@elangley.org>
2022-11-13 14:49:29 -0800

    feat: add expansion

--- shouldiusethisacronym.lisp
+++ shouldiusethisacronym.lisp
@@ -6,6 +6,17 @@
 (defclass acceptor (hunchentoot:acceptor)
   ())
 
+(defun lookup-acronym (acronym)
+  (string-case:string-case ((string-downcase acronym) :default nil)
+    ("aiui" "As I Understand It")
+    ("afaict" "As Far As I Can Tell")
+    ("iow" "In Other Words")
+    ("lotr" "Lord of the Rings")
+    ("n/a" "Not Applicable")
+    ("tl;dr" "To Long; Didn't Read")
+    ("tldr" "To Long; Didn't Read")
+    ))
+
 (defmethod hunchentoot:acceptor-dispatch-request ((acceptor acceptor) request)
   (let ((script-name (hunchentoot:script-name request)))
     (cond ((or (equal script-name "")
@@ -24,7 +35,9 @@
                           ))
                  (:h1  "Should I use this acronym?")
                  (:form :action "/" :method "post"
-                        (:input  :name "acronym" :style "margin-top: 1em; font-size: 2em;")))))
+                        (:input  :name "acronym" :style "margin-top: 1em; font-size: 2em;")
+                        (:button :type "submit" :style "font-size: 2em;"
+                                 "?")))))
           ((> (length script-name)
               1)
            (let* ((acronym (subseq (hunchentoot:script-name request) 1))
@@ -38,8 +51,11 @@
                         "body {height: 50%; width: 75%; margin: 25% auto;}"
                         ))
                (:h1 question)
-               (:div :style "padding-top: 1em; font-size: 2em;"
-                     "Don't use acronyms; they impede signification and thought.")))))))
+               (:div :style "padding-top: 2rem; font-size: 2em;"
+                     "Don't use acronyms; they impede signification and thought.")
+               (alexandria:when-let ((expansion (lookup-acronym acronym)))
+                 (:div :style "padding-top: 2rem;"
+                       ("You might use \"~a\" instead!" expansion)))))))))
 
 (defun main ()
   (uiop:setup-command-line-arguments)
