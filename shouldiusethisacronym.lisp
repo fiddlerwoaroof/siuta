2bf39b4849dc9bca908c94250097b99471fdba4c
Edward Langley <el-github@elangley.org>
2022-11-13 14:39:43 -0800

    feat: add main page

--- shouldiusethisacronym.lisp
+++ shouldiusethisacronym.lisp
@@ -7,21 +7,39 @@
   ())
 
 (defmethod hunchentoot:acceptor-dispatch-request ((acceptor acceptor) request)
-  (when (> (length (hunchentoot:script-name request))
-           1)
-    (let* ((acronym (subseq (hunchentoot:script-name request) 1))
-           (question (format nil "Should I use the acronym \"~a\"?" acronym)))
-      (spinneret:with-html-string
-        (:head
-         (:meta :property "og:title" :content question)
-         (:meta :property "og:description" :content "No.")
-         (:style :type "text/css"
-                 "html {font-family: sans-serif; width: 100vw; height: 100vh;}"
-                 "body {height: 50%; width: 75%; margin: 25% auto;}"
-                 ))
-        (:h1 question)
-        (:div :style "padding-top: 1em; font-size: 2em;"
-              "Don't use acronyms; they impede signification and thought.")))))
+  (let ((script-name (hunchentoot:script-name request)))
+    (cond ((or (equal script-name "")
+               (equal script-name "/"))
+           (if (eql :post
+                    (hunchentoot:request-method request))
+               (hunchentoot:redirect (format nil "/~a"
+                                             (hunchentoot:post-parameter "acronym" request)))
+               (spinneret:with-html-string
+                 (:head
+                  (:meta :property "og:title" :content "Should I use this acronym?")
+                  (:meta :property "og:description" :content "No.")
+                  (:style :type "text/css"
+                          "html {font-family: sans-serif; width: 100vw; height: 100vh;}"
+                          "body {height: 50%; width: 75%; margin: 25% auto;}"
+                          ))
+                 (:h1  "Should I use this acronym?")
+                 (:form :action "/" :method "post"
+                        (:input  :name "acronym" :style "margin-top: 1em; font-size: 2em;")))))
+          ((> (length script-name)
+              1)
+           (let* ((acronym (subseq (hunchentoot:script-name request) 1))
+                  (question (format nil "Should I use the acronym \"~a\"?" acronym)))
+             (spinneret:with-html-string
+               (:head
+                (:meta :property "og:title" :content question)
+                (:meta :property "og:description" :content "No.")
+                (:style :type "text/css"
+                        "html {font-family: sans-serif; width: 100vw; height: 100vh;}"
+                        "body {height: 50%; width: 75%; margin: 25% auto;}"
+                        ))
+               (:h1 question)
+               (:div :style "padding-top: 1em; font-size: 2em;"
+                     "Don't use acronyms; they impede signification and thought.")))))))
 
 (defun main ()
   (uiop:setup-command-line-arguments)
